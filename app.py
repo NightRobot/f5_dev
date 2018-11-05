efc3c0dde22e7f161c8faf1f4a6beeae531d72d4
NightRobot <alseus13@hotmail.com>
2018-11-05 11:55:08 +0700

    add time delay

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from f5.bigip import ManagementRoot
 from pprint import pprint
-
+import time ,os
+from time import gmtime, strftime
 ipaddress = '172.16.70.105'
 username = 'admin'
 password = 'password'
+pool_name = "test"
 # connect to BIGIP management server
 mgmt = ManagementRoot(ipaddress, username, password)
 
@@ -17,33 +19,39 @@ print("---------------------------------------------------")
 pools = mgmt.tm.ltm.pools
 # Resource pool
 pool = mgmt.tm.ltm.pools.pool
-pool_collection = pools.get_collection()
-for p in pool_collection :
-    print("pool name : ",p.name)
-    print("loadbalance method : ",p.loadBalancingMode)
-    # load pool name 'test' from resource pool
-    pool_test = pool.load(partition='Common', name=p.name)
-    
-    # REST api => https://172.16.70.105/mgmt/tm/ltm/pool/~Common~<pool name>/members
-    # Collection member
-    members = pool_test.members_s
-    # Resource member
-    member = pool_test.members_s.members
-    print("==> member collection in ",p.name," pool <==")
-    member_collection = members.get_collection()
-    print(member_collection)
-    for m in member_collection :
+# load pool name 'test' from resource pool
+pool_test = pool.load(partition='Common', name=pool_name)
+if __name__ == "__main__" :
+    # for p in pool_collection :
+    while(True) :
+        print("pool name : ",pool_test.name)
+        print("loadbalance method : ",pool_test.loadBalancingMode)
+        
+        # REST api => https://172.16.70.105/mgmt/tm/ltm/pool/~Common~<pool name>/members
+        # Collection member
+        members = pool_test.members_s
+        # Resource member
+        member = pool_test.members_s.members
+        print("==> member collection in ",pool_test.name," pool <==")
+        member_collection = members.get_collection()
+        print(member_collection)
+        for m in member_collection :
 
-        # REST api => https://172.16.70.105/mgmt/tm/ltm/pool/~Common~<pool name>/members/~Common~<member name>
-        print("name : ",m.name)
-        print("address : ",m.address)
-        print("ratio : ",m.ratio)
-        print("session : ",m.session)
-        print("state : ",m.state)
+            # REST api => https://172.16.70.105/mgmt/tm/ltm/pool/~Common~<pool name>/members/~Common~<member name>
+            print("name : ",m.name)
+            print("address : ",m.address)
+            print("ratio : ",m.ratio)
+            print("session : ",m.session)
+            print("state : ",m.state)
+            print("---------------------------------------------------")
+            # m.ratio = 2
+            # m.update()
         print("---------------------------------------------------")
-        # m.ratio = 2
-        # m.update()
-    print("---------------------------------------------------")
+        localtime = time.asctime( time.localtime(time.time()) )
+        print ("Local current time :", localtime)
+        time.sleep(60)
+        
+        
 
 
 
