c6514445cb86140cbf7916fc97006dd550970f58
NightRobot <alseus13@hotmail.com>
2018-11-05 08:57:34 +0700

    change output print data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from f5.bigip import ManagementRoot
 from pprint import pprint
+
 ipaddress = '172.16.70.105'
 username = 'admin'
 password = 'password'
@@ -8,28 +9,42 @@ mgmt = ManagementRoot(ipaddress, username, password)
 
 # https://172.16.70.105/mgmt/tm/ltm/
 ltm = mgmt.tm.ltm
-
+print("---------------------------------------------------")
+print ("management server ",ipaddress)
+print("---------------------------------------------------")
 # REST api => https://172.16.70.105/mgmt/tm/ltm/pool
 # Collection pool
 pools = mgmt.tm.ltm.pools
 # Resource pool
 pool = mgmt.tm.ltm.pools.pool
+pool_collection = pools.get_collection()
+for p in pool_collection :
+    print("pool name : ",p.name)
+    print("loadbalance method : ",p.loadBalancingMode)
+    # load pool name 'test' from resource pool
+    pool_test = pool.load(partition='Common', name=p.name)
+    
+    # REST api => https://172.16.70.105/mgmt/tm/ltm/pool/~Common~<pool name>/members
+    # Collection member
+    members = pool_test.members_s
+    # Resource member
+    member = pool_test.members_s.members
+    print("==> member collection in ",p.name," pool <==")
+    member_collection = members.get_collection()
+    print(member_collection)
+    for m in member_collection :
 
-# load pool name 'test' from resource pool
-pool_test = pool.load(partition='Common', name='test')
+        # REST api => https://172.16.70.105/mgmt/tm/ltm/pool/~Common~<pool name>/members/~Common~<member name>
+        print("name : ",m.name)
+        print("address : ",m.address)
+        print("ratio : ",m.ratio)
+        print("session : ",m.session)
+        print("state : ",m.state)
+        print("---------------------------------------------------")
+        # m.ratio = 2
+        # m.update()
+    print("---------------------------------------------------")
 
-# REST api => https://172.16.70.105/mgmt/tm/ltm/pool/~Common~test/members
-# Collection member
-members = pool_test.members_s
-# Resource member
-member = pool_test.members_s.members
 
-member_collection = members.get_collection()
-for m in member_collection :
-    pprint(m.raw)
-    m.ratio = 2
-    m.update()
-    # member_obj = pool_test.members_s.members.load(partition='Common', name=m.name)
-    # print(member_obj.ratio)
 
 
